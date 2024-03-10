c824dcbb249e8f8cba4d19e329d92b4d0c1719e7
kirkbyers <kirk@cratejoy.com>
2024-03-09 22:00:47 -0600

    refactor: enable concurent app runs for tests.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind to port 8000");
+    run(listener)?.await
 }
