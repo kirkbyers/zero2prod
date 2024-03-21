71ea96e4ec31c29cf70822f860d65eb7843a6429
kirkbyers <kirk@cratejoy.com>
2024-03-21 15:59:46 -0500

    Fmt+Lint.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use serde::Deserialize;
 use uuid::Uuid;
-use chrono::Utc;
 
 #[derive(Deserialize)]
 #[allow(dead_code)]
@@ -9,7 +9,6 @@ pub struct SubscriberData {
     email: String,
     name: String,
 }
-
 
 const INSERT_QUERY: &str = r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -27,14 +26,18 @@ pub async fn subscribe(
         .await
         .expect("Failed to prepare query.");
     match stmt
-        .execute((id.to_string(), json.email.clone(), json.name.clone(), now.to_rfc3339()))
-        .await {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        },
+        .execute((
+            id.to_string(),
+            json.email.clone(),
+            json.name.clone(),
+            now.to_rfc3339(),
+        ))
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             eprintln!("Failed to execute query: {:?}", e);
-            return HttpResponse::InternalServerError().finish();
+            HttpResponse::InternalServerError().finish()
         }
     }
 }
