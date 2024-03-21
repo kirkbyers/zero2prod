7d89c4c76476be24e81a675ad34bb232e96690ba
kirkbyers <kirk@cratejoy.com>
2024-03-21 15:58:42 -0500

    Save POST /subscriptions.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use uuid::Uuid;
+use chrono::Utc;
 
 #[derive(Deserialize)]
 #[allow(dead_code)]
@@ -8,9 +10,31 @@ pub struct SubscriberData {
     name: String,
 }
 
+
+const INSERT_QUERY: &str = r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES (?1, ?2, ?3, ?4);
+"#;
+
 pub async fn subscribe(
-    _json: web::Json<SubscriberData>,
-    _connection: web::Data<libsql::Connection>,
+    json: web::Json<SubscriberData>,
+    connection: web::Data<libsql::Connection>,
 ) -> HttpResponse {
-    HttpResponse::Ok().finish()
+    let id = Uuid::new_v4();
+    let now = Utc::now();
+    let mut stmt = connection
+        .prepare(INSERT_QUERY)
+        .await
+        .expect("Failed to prepare query.");
+    match stmt
+        .execute((id.to_string(), json.email.clone(), json.name.clone(), now.to_rfc3339()))
+        .await {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
+        Err(e) => {
+            eprintln!("Failed to execute query: {:?}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    }
 }
