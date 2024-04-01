2496bb1b5a450a26b61ab1470ea3990d678cd432
kirkbyers <kirk@cratejoy.com>
2024-03-31 20:35:20 -0500

    Init job runner in main.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use actix_web::rt;
 use std::net::TcpListener;
 
 use zero2prod::{configuration::get_configuration, startup::run};
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    rt::spawn(async {
+        loop {
+            println!("Hello from Tokio!");
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+        }
+    });
+
     let config =
         get_configuration(Some("configuration.yaml")).expect("Failed to read configuration.");
     let address = format!("0.0.0.0:{}", config.application_port);
