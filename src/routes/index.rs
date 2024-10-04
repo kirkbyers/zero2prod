39e3c094143c024859f8a73a10317c94a93b6f37
Kirk Byers <kirklbyers@gmail.com>
2024-10-03 22:31:24 -0500

    Only return the latest scrapes.

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,8 +1,9 @@
 use actix_web::{get, web, HttpResponse};
+use libsql::params;
 use tera::{Context, Tera};
 
 use crate::{
-    models::{jobs, scrape},
+    models::scrape,
     routes::scrapes,
 };
 
@@ -10,37 +11,22 @@ use crate::{
 async fn home(conn: web::Data<libsql::Connection>) -> HttpResponse {
     let mut tera_context = Context::new();
 
-    let recent_scrape_job = jobs::select_with_pagination(
-        "created_at, completed_at",
-        &format!("job_type = {}", jobs::JobType::SMScrape.as_i32()),
-        "created_at",
-        "DESC",
-        1,
-        0,
-    );
-    let scrape_row = match conn.get_ref().query(&recent_scrape_job, ()).await {
-        Ok(mut row) => match row.next().await {
-            Ok(row) => row,
-            Err(e) => {
-                eprintln!("Error: {:?}", e);
-                return HttpResponse::InternalServerError().body("Error parsing db results");
-            }
-        },
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            return HttpResponse::InternalServerError().body("Error querying the database");
-        }
-    };
-    let scrape_created_at: String = match scrape_row {
-        Some(scrape) => scrape.get::<String>(0).unwrap_or(String::new()),
-        None => String::new(),
+    let mut max_batch_id_rows = conn
+        .query("SELECT COALESCE(MAX(batch_id), 0) FROM scrapes", params![])
+        .await
+        .expect("Failed to query max batch_id");
+
+    let max_batch_id: i32 = if let Ok(Some(row)) = max_batch_id_rows.next().await {
+        row.get(0).unwrap()
+    } else {
+        0
     };
 
     let select_page = scrape::select_with_pagination(
         "id, url, arrival, lot_size, bag_size, score, packaging, cultivar_detail, spro_rec",
         &format!(
-            "score != '' AND strftime(scraped_at) > strftime('{}')",
-            scrape_created_at
+            "score != '' AND batch_id = {}",
+            max_batch_id
         ),
         "score",
         "DESC",
